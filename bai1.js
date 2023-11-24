5a2e576f0d2384b59cef6e7461330c11cec2ebed
NguyenLannnFE <125577532+hoanglan03112003@users.noreply.github.com>
2023-11-24 18:17:59 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,47 @@
+const ListItem = document.querySelectorAll("li");
+
+ListItem.forEach((item) => {
+  item.addEventListener("mouseover", function () {
+    this.style.backgroundColor = "#f2f2f2";
+    this.style.color = "black";
+  });
+  item.addEventListener("mouseout", function () {
+    this.style.backgroundColor = "black";
+    this.style.color = "white";
+  });
+});
+
+const AddItem = document.getElementById("demo3");
+const ShowFeatures = document.getElementById("showlogin");
+const CloseLogin = document.getElementById("test2");
+const CloseLogin2 = document.getElementById("test3");
+
+/// show and close login
+AddItem.addEventListener("click", function () {
+  ShowFeatures.style.display = "block";
+});
+
+CloseLogin.addEventListener("click", function () {
+  ShowFeatures.style.display = "none";
+});
+CloseLogin2.addEventListener("click", function () {
+  ShowFeatures.style.display = "none";
+});
+
+const LoginForm = document.getElementById("test5");
+LoginForm.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "nguyenhoanglan" && password === "123456") {
+    alert("Logged in successfully");
+  } else if (username !== "nguyenhoanglan") {
+    alert("Wrong account");
+  } else if (password !== "123456") {
+    alert("Wrong password");
+  } else {
+    alert("Login failed");
+  }
+});
